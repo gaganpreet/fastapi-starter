ac4d5a88fa9d3727da8cc3b5fee9780d7c842ac7
Gaganpreet Arora <me@gaganpreet.in>
2021-09-01 23:01:18 +0200

    Add secret key to env

--- {{cookiecutter.project_slug}}/backend/app/core/config.py
+++ {{cookiecutter.project_slug}}/backend/app/core/config.py
@@ -3,12 +3,21 @@ from pydantic import BaseSettings, HttpUrl, PostgresDsn
 
 
 class Settings(BaseSettings):
+
     PROJECT_NAME: str = "{{cookiecutter.project_name}}"
 
-    DATABASE_URL: PostgresDsn
     SENTRY_DSN: Optional[HttpUrl] = None
 
     API_PATH: str = "/api/v1"
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 7 * 24 * 60  # 7 days
+
+    # The following variables need to be defined in environment
+
+    DATABASE_URL: PostgresDsn
+    SECRET_KEY: str
+
+    #  END: required environment variables
 
 
 settings = Settings()

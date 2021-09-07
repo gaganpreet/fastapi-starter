db3a2f0206ff495a5a5d44241a00d63ac28b2fb9
Gaganpreet Arora <me@gaganpreet.in>
2021-09-07 22:42:26 +0200

    Add more config variables

--- {{cookiecutter.project_slug}}/backend/app/core/config.py
+++ {{cookiecutter.project_slug}}/backend/app/core/config.py
@@ -1,5 +1,7 @@
-from typing import Optional
-from pydantic import BaseSettings, HttpUrl, PostgresDsn
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseSettings, HttpUrl, PostgresDsn, root_validator, validator
+from pydantic.networks import AnyHttpUrl
 
 
 class Settings(BaseSettings):
@@ -12,12 +14,21 @@ class Settings(BaseSettings):
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 7 * 24 * 60  # 7 days
 
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
     # The following variables need to be defined in environment
 
     DATABASE_URL: PostgresDsn
-    SECRET_KEY: str
+    TEST_DATABASE_URL: Optional[PostgresDsn]
 
+    SECRET_KEY: str
     #  END: required environment variables
+
+    @root_validator
+    def validate_database_url(cls, values):
+        # Either one of the database urls need to be set
+        assert values.get("DATABASE_URL") or values.get("TEST_DATABASE_URL")
+        return values
 
 
 settings = Settings()

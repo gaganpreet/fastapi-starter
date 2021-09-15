7acf592f994dbd2b7eb9a9b25bbe92a79e1a24cd
Gaganpreet Arora <me@gaganpreet.in>
2021-09-15 10:41:44 +0200

    Create nginx docker build for frontend

--- {{cookiecutter.project_slug}}/frontend/src/App.test.tsx
+++ {{cookiecutter.project_slug}}/frontend/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to admin/i);
   expect(linkElement).toBeInTheDocument();
 });

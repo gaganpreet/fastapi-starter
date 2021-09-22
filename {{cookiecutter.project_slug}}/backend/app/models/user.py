d0148cec4d1dd808380a59e4212ce814c8f68d4f
Gaganpreet Arora <me@gaganpreet.in>
2021-09-22 23:41:57 +0200

    Fix model

--- {{cookiecutter.project_slug}}/backend/app/models/user.py
+++ {{cookiecutter.project_slug}}/backend/app/models/user.py
@@ -1,6 +1,5 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable
 from sqlalchemy import Column, DateTime
-from sqlalchemy.orm import relationship
 from sqlalchemy.sql.functions import func
 
 from app.db import Base
@@ -13,7 +12,6 @@ class User(Base, SQLAlchemyBaseUserTable):
     updated = Column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
-    items = relationship("Item", back_populates="user", cascade="all, delete")
 
     def __repr__(self):
         return f"User(id={self.id!r}, name={self.email!r})"

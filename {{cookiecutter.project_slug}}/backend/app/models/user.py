1a6162e707013ddc0019b42aec353f1e97d21e81
Gaganpreet Arora <me@gaganpreet.in>
2021-09-22 23:31:59 +0200

    Migrate to fastapi users 8.x

--- {{cookiecutter.project_slug}}/backend/app/models/user.py
+++ {{cookiecutter.project_slug}}/backend/app/models/user.py
@@ -1,9 +1,9 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users.db import SQLAlchemyBaseUserTable
 from sqlalchemy import Column, DateTime
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.functions import func
 
-from app.db import Base, database
-from app.schemas.user import UserDB
+from app.db import Base
 
 
 class User(Base, SQLAlchemyBaseUserTable):
@@ -13,10 +13,7 @@ class User(Base, SQLAlchemyBaseUserTable):
     updated = Column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
+    items = relationship("Item", back_populates="user", cascade="all, delete")
 
     def __repr__(self):
         return f"User(id={self.id!r}, name={self.email!r})"
-
-
-users = User.__table__
-user_db = SQLAlchemyUserDatabase(UserDB, database, users)

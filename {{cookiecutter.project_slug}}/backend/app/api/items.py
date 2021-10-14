7bf0bac7c07e322edea4daf37c3cd15763428e39
Gaganpreet Arora <me@gaganpreet.in>
2021-10-14 22:02:56 +0200

    Remove test code

--- {{cookiecutter.project_slug}}/backend/app/api/items.py
+++ {{cookiecutter.project_slug}}/backend/app/api/items.py
@@ -1,10 +1,8 @@
-import json
 from typing import Any, List, Optional
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import func, select
 from sqlalchemy.orm.session import Session
-from sqlalchemy.sql import asc, desc
 from starlette.responses import Response
 
 from app.deps.db import get_db
@@ -17,26 +15,6 @@ from app.schemas.item import ItemCreate
 from app.schemas.request_params import RequestParams
 
 router = APIRouter(prefix="/items")
-
-
-def parse_request(model, sort_, range_):
-    skip, limit = 0, 10
-    if range_:
-        start, end = json.loads(range_)
-        skip, limit = start, (end - start + 1)
-
-    order_by = desc(model.id)
-    if sort_:
-        sort_column, sort_order = json.loads(sort_)
-        if sort_order.lower() == "asc":
-            direction = asc
-        elif sort_order.lower() == "desc":
-            direction = desc
-        else:
-            raise HTTPException(400, f"Invalid sort direction {sort_order}")
-        order_by = direction(model.__table__.c[sort_column])
-
-    return skip, limit, order_by
 
 
 @router.get("/", response_model=List[ItemSchema])

0c03d2e373218c8f0563249d0b48e7d1b64dc74c
Gaganpreet Arora <me@gaganpreet.in>
2022-05-18 00:47:26 +0200

    Run genapi in tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -28,3 +28,7 @@ docker-compose exec -T backend alembic upgrade head
 docker build --target build -t frontend-build:latest frontend
 
 docker run --network host frontend-build bash -c "apt-get update && apt-get install -qq xvfb libnss3 libatk1.0 libatk-bridge2.0 libgtk-3.0 libgbm1 libasound2 && yarn run-e2e-tests"
+
+cd ./frontend/
+
+yarn genapi

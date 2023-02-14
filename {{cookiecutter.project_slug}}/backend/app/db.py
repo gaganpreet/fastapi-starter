52d04242ad5ba98e7992e65ce0069a6d471a6b8c
Gaganpreet <me@gaganpreet.in>
2023-02-14 12:05:17 +0100

    Remove databases module, not needed anymore (#395)

--- {{cookiecutter.project_slug}}/backend/app/db.py
+++ {{cookiecutter.project_slug}}/backend/app/db.py
@@ -1,4 +1,3 @@
-import databases
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
@@ -21,5 +20,3 @@ SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 mapper_registry = registry()
 Base: DeclarativeMeta = declarative_base()
-
-database = databases.Database(settings.DATABASE_URL)

1fe9f899429da32d70f043128ae80399de9aaecf
Gaganpreet Arora <me@gaganpreet.in>
2023-02-23 23:51:08 +0100

    debug print

--- scripts/test.sh
+++ scripts/test.sh
@@ -27,12 +27,12 @@ docker-compose exec -T backend alembic upgrade head
 
 docker build --target build -t frontend-build:latest frontend
 
-docker run --network host frontend-build bash -c "apt-get update && apt-get install -qq xvfb libnss3 libatk1.0 libatk-bridge2.0 libgtk-3.0 libgbm1 libasound2 && yarn run-e2e-tests"
+docker run --network host frontend-build bash -v $(pwd)/frontend/cypress/:/app/cypress -c "apt-get update && apt-get install -qq xvfb libnss3 libatk1.0 libatk-bridge2.0 libgtk-3.0 libgbm1 libasound2 && yarn run-e2e-tests"
 
 # Bind mount src/generated directory and fail if it changed
 # This is to ensure that the generated files are always in sync with FastAPI code
 cp -ruv ./frontend/src/generated /tmp/src-generated
 
-docker run --network host frontend-build -v $(pwd)/frontend/src/generated:/app/src/generated -v $(pwd)/frontend/cypress/:/app/cypress bash -c "apt-get update && apt-get install -qq default-jre && yarn config set script-shell /bin/bash && yarn genapi"
+docker run --network host frontend-build -v $(pwd)/frontend/src/generated:/app/src/generated bash -c "apt-get update && apt-get install -qq default-jre && yarn config set script-shell /bin/bash && yarn genapi"
 
 diff -r /tmp/src-generated ./frontend/src/generated || (echo "Generated files changed. Please make sure they are in sync" && exit 1)

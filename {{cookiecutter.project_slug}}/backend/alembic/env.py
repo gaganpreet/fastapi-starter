d042fb43f6382efe073b84473aab66bd31b6e630
Gaganpreet Arora <me@gaganpreet.in>
2023-02-27 09:48:15 +0100

    fix alembic import

--- {{cookiecutter.project_slug}}/backend/alembic/env.py
+++ {{cookiecutter.project_slug}}/backend/alembic/env.py
@@ -4,7 +4,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
-from app.models import Base
+from app.db import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.

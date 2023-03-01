e265e9e0db405daf6ed9234b66253d674d857013
Gaganpreet Arora <me@gaganpreet.in>
2023-03-01 23:06:45 +0100

    Use async session maker

--- {{cookiecutter.project_slug}}/backend/app/db.py
+++ {{cookiecutter.project_slug}}/backend/app/db.py
@@ -1,12 +1,13 @@
 from typing import Any
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
-async_engine = create_async_engine(settings.ASYNC_DATABASE_URL)
-async_session_maker = sessionmaker(
+async_engine = create_async_engine(str(settings.ASYNC_DATABASE_URL))
+
+async_session_maker = async_sessionmaker(
     async_engine,
     class_=AsyncSession,
     expire_on_commit=False,

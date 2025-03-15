13c0e402dcbe269fad010ac0cf9645595e71ed03
Gaganpreet Arora <me@gaganpreet.in>
2025-03-15 13:08:20 +0100

    fix test script, remove extra package

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 
 set -eou pipefail
 
-# Stop containers from previous run, if any
+# Stop containers from prlibasound2evious run, if any
 (cd ./test-project && docker compose -f docker-compose.yml down --rmi local -v && cd ..) || true
 
 # Run this from the root of the project
@@ -30,7 +30,7 @@ docker build --target build -t frontend-build:latest frontend
 
 mv $(pwd)/frontend/src/generated /tmp/src-generated
 
-PACKAGE_LIST="xvfb libnss3 libatk1.0 libatk-bridge2.0 libgtk-3.0 libgbm1 libasound2 default-jre"
+PACKAGE_LIST="xvfb libnss3 libatk1.0 libatk-bridge2.0 libgtk-3.0 libgbm1 default-jre"
 # If GITHUB_REPOSITORY is not set, then we can just run it in docker
 # We need to have two paths here because Github CI works weirdly with bind mounts that we can use to run the tests locally in Docker
 if [ -z "${GITHUB_REPOSITORY-}" ]
